db3f4e30bb29a0a8daa58f9a0ad39e241a5d80c2
Ben Tan <bentan123@hotmail.com>
2024-06-10 23:09:28 +0800

    add Preact component, use client: directive for hydration on UI framework component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://resplendent-kashata-a93e45.netlify.app"
+  site: "https://resplendent-kashata-a93e45.netlify.app",
+  integrations: [preact()]
 });
